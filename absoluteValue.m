a2f5ca4d6bfc63392ff55dcc48994981844032c5
Momin-C <hellomomins@yahoo.com>
2023-11-21 21:48:10 -0500

    Absolute value

--- absoluteValue.m
+++ absoluteValue.m
@@ -1,5 +1,14 @@
-function [outputArg1] = absoluteValue(inputSoundArray)
+function [absArray] = absoluteValue(inputSoundArray)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-    outputArg1 = abs(inputSoundArray);
+    
+%outputArg1 = abs(inputSoundArray);
+
+    absArray = zeros(rows, cols);
+
+    for i = 1:rows
+        for j = 1:cols
+            absArray(i, j) = abs(inputSoundArray(i, j));
+        end
+    end
 end
